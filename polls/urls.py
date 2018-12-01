d72ae14cb142f3b2004d7f4eee6704bc6eeb1928
ehuan015 <ehuan015@gmail.com>
2018-11-30 20:50:09 -0800

    redid polling

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name="polls"
 urlpatterns = [
-    path('list/', views.polls_list),
+    path('list/', views.polls_list, name="list"),
+    path('details/<int:poll_id>/', views.poll_detail, name='detail'),
+    path('details/<int:poll_id>/vote/', views.poll_vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+
+from .models import Poll, Choice
 
 # Create your views here.
 def polls_list(request):
-    return render(request, 'polls_list.html')
+    """
+        renders polls_list.html which lists all available polls
+    """
+    polls = Poll.objects.all()
+    context = {'polls': polls}
+    return render(request, 'polls_list.html', context)
+
+def poll_detail(request, poll_id):
+    """
+        render poll
+    """
+    #poll = Poll.objects.get(id=poll_id)
+    poll = get_object_or_404(Poll, id=poll_id)
+    context = {'poll': poll}
+    return render(request, 'poll_detail.html', context)
+
+def poll_vote(request, poll_id):
+    choice_id = request.POST.get('choice')
+    if choice_id:
+        choice = Choice.objects.get(id=poll_id)
+        poll = choice.question
+        choice.votes += 1
+        choice.save()
+        return render(request, 'poll_results.html', {'poll': poll})
+    return render(request, 'poll_results.html', {'error': True})
