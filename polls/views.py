88373db76afd374c34386aad162cf85863f19516
ehuan015 <ehuan015@gmail.com>
2018-12-01 14:09:01 -0800

    starting on login function

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib import messages
+from django.urls import reverse
 
 from .models import Poll, Choice
 
@@ -22,11 +24,13 @@ def poll_detail(request, poll_id):
     return render(request, 'poll_detail.html', context)
 
 def poll_vote(request, poll_id):
+    poll = get_object_or_404(Poll, id=poll_id)
     choice_id = request.POST.get('choice')
     if choice_id:
         choice = Choice.objects.get(id=poll_id)
-        poll = choice.question
         choice.votes += 1
         choice.save()
-        return render(request, 'poll_results.html', {'poll': poll})
-    return render(request, 'poll_results.html', {'error': True})
+    else:
+        messages.error(request, 'No choice was made.')
+        return HttpResponseRedirect(reverse("polls:detail", args=(poll_id,)))
+    return render(request, 'poll_results.html', {'poll': poll})
